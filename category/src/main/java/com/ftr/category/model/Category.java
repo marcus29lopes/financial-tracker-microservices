6150874cebe20f55c066ee3292cc2695a264a526
marcus <marcuslopee@gmail.com>
2025-09-21 10:17:37 -0300

    feat: entidade category mapeada

---
+++ category/src/main/java/com/ftr/category/model/Category.java
@@ -0,0 +1,26 @@
+package com.ftr.category.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @Column(name  = "name", nullable = false)
+    private String name;
+
+    @Column(name  = "createdAt")
+    private LocalDateTime createdAt;
+}
