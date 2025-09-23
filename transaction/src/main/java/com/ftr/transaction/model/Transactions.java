412a2b2b30f74bb31f424aa31e881027ed4e56fa
marcus <marcuslopee@gmail.com>
2025-09-23 16:28:18 -0300

    feat: transaction mapeada

---
+++ transaction/src/main/java/com/ftr/transaction/model/Transactions.java
@@ -0,0 +1,58 @@
+package com.ftr.transaction.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "transactions")
+public class Transactions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transaction_id")
+    private Long transactionId;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @Column(name = "category_name")
+    private Long categoryName;
+
+    @Column(name = "type_id")
+    private Long typeId;
+
+    @Column(name = "type_name")
+    private String typeName;
+
+    @Column(name = "value")
+    private Long value;
+
+    @Column(name = "receipt_url")
+    private String receiptUrl;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "description")
+    private String description;
+
+
+
+
+
+
+}
--- transaction/src/main/resources/db/migration/V1__create_table_transaction.sql
+++ transaction/src/main/resources/db/migration/V1__create_table_transaction.sql
@@ -2,15 +2,13 @@ CREATE TABLE transactions (
     transaction_id SERIAL PRIMARY KEY,
     user_id VARCHAR,
     user_email VARCHAR,
-    categoria_id VARCHAR NOT NULL ,
-    categoria_name VARCHAR,
+    category_id VARCHAR NOT NULL ,
+    category_name VARCHAR,
     type_id VARCHAR,
     type_name VARCHAR,
     value NUMERIC(15,2) NOT NULL,
     receipt_url TEXT,
     created_at TIMESTAMP DEFAULT NOW(),
     description TEXT
-
-
 
 );
